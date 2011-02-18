fa708a1962dcdc4be02d67be2ea8a3f55b33c76f
liesbeth <liesbethvanderplas@gmail.com>
2011-02-18 08:51:49 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sommenfabriek::Application.config.secret_token = '75fbca8500e974f21b74b5ec7e9d9a091ddaa1e3735cd30f2b37a8e59c9275d850fac37dc4152a5e9f2e628f0b7afb80389e2dd9c461fc3e89b1e4f966a40319'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sommenfabriek::Application.config.session_store :cookie_store, :key => '_sommenfabriek_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sommenfabriek::Application.config.session_store :active_record_store
