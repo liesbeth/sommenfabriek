2821f4350031e168aedaa8283d35f02580fa740e
liesbeth <liesbethvanderplas@gmail.com>
2011-02-19 10:20:04 +0100

    Done with homepages

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,11 +1,14 @@
 class HomepagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def contents
+    @title = "Contents"
   end
 
 end
