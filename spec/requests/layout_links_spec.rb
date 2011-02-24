9b385c23f0c1a39be15b331bfe13e1ffd79623db
liesbeth <liesbethvanderplas@gmail.com>
2011-02-24 12:42:14 +0100

    Finished layout routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have a Contents page at '/contents'" do
+    get '/contents'
+    response.should have_selector('title', :content => "Contents")
+  end
+  
+end
